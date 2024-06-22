62a80b86646af4b66855f25fbadaa2108cb95e1b
Stanislav Minenko <54997035+Cheeezcake@users.noreply.github.com>
2024-06-22 16:35:48 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // swiftlint is kinda big to pull in and build right now...maybe later
 //        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMajor(from: "0.52.0")),
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.1.1"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2"),
     ],
     targets: [
         .target(
